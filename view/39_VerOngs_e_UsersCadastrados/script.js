4df9bb81094747fb88dbf93f46305f7509d667c1
Allan Fernandes <allandossantosfernandes@gmail.com>
2025-02-21 21:38:50 -0400

    Icones com js adicionado

--- view/39_VerOngs_e_UsersCadastrados/script.js
+++ view/39_VerOngs_e_UsersCadastrados/script.js
@@ -1,16 +1,30 @@
-const icone = document.getElementById("status-icone");
+const icone_aguardando = document.getElementById("status-aguardando");
+const icone_aprovado = document.getElementById("status-aprovado");
+const icone_recusado = document.getElementById("status-recusado");
 
-switch (icone) {
-  case "<td id='status-icone'>Aguardando</td>":
-    icone.innerHTML =
-      "<td id ='status-icone'>Agurdando <span class='material-symbols-outlined'>hourglass_empty</span></td>";
-    break;
-  case "<td id='status-icone'>Aprovado</td>":
-    icone.innerHTML =
-      "<td id ='status-icone'>Agurdando <span class='material-symbols-outlined'>check</span></td>";
-    break;
-  case "<td id='status-icone'>Recusado</td>":
-    icone.innerHTML =
-      "<td id ='status-icone'>Agurdando <span class='material-symbols-outlined'>close</span></td>";
-    break;
+// console.log(icone);
+
+let array = ["Aguardando", "Recusado", "Aprovado"];
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+
+  // icone.innerText.trim()
+
+  console.log(element);
+
+  switch (element) {
+    case "Aguardando":
+      icone_aguardando.innerHTML =
+        "Aguardando <span class='material-symbols-outlined'>hourglass_empty</span>";
+      break;
+    case "Aprovado":
+      icone_aprovado.innerHTML =
+        "Aprovado <span class='material-symbols-outlined'>check</span>";
+      break;
+    case "Recusado":
+      icone_recusado.innerHTML =
+        "Recusado <span class='material-symbols-outlined'>close</span>";
+      break;
+  }
 }
