7c0a22286d46ad9ee70183a7be583b23db579087
Marcus Prado <marcus6599846@aluno.ms.senac.br>
2025-02-25 22:00:37 -0400

    slider footer

--- view/assests/js/sobreNos.js
+++ view/assests/js/sobreNos.js
@@ -1,14 +1,24 @@
 const slides = document.querySelector('.slides');
 const slide = document.querySelectorAll('.slide');
+const totalSlides = slide.length;
 let currentIndex = 0;
 
 function showSlide(index) {
     slides.style.transform = `translateX(${-index * 100}%)`;
+    slides.style.transition = 'transform 0.5s ease'; // Aplica a transição suavemente
 }
 
 function nextSlide() {
-    currentIndex = (currentIndex + 1) % slide.length;
+    currentIndex++;
     showSlide(currentIndex);
+
+    if (currentIndex >= totalSlides - 1) {
+        setTimeout(() => {
+            slides.style.transition = 'none';
+            slides.style.transform = 'translateX(0%)';
+            currentIndex = 0;
+        }, 500);
+    }
 }
 
 setInterval(nextSlide, 3000);
