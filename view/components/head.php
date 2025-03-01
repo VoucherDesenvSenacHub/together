9c572c90ed2c6c2e6b2dd76a14e3c1b6126a12ba
Rhyan Komm <rhyankomm@hotmail.com>
2025-02-28 20:02:39 -0400

    att

--- view/components/head.php
+++ view/components/head.php
@@ -4,5 +4,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Together</title>
-    <link rel="stylesheet" href="/together/view/assets/css/style.css">
+    <link rel="stylesheet" href="../../assests/css/style.css">
 </head>
