44b42a9939434ecb0e926b541f2958a7db51870f
Maria Avila <maria54099016@aluno.ms.senac.br>
2025-02-28 21:32:07 -0400

    inicindo task

---
+++ view/pages/Ong/visualizarVoluntarios.php
@@ -0,0 +1,138 @@
+<?php 
+include __DIR__ . "/../../components/head.php";
+?>
+
+
+<body>
+    <main>
+        <header>
+            <?php
+                include __DIR__ . "/../../components/navbar.php"
+            ?>
+        </header>
+    <div class="container-h1 texto-gradient">
+        <h1>Validação de ONGs</h1>
+    </div>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Nome da ONG</th>
+                    <th>Status</th>
+                    <th>Visualizar</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>XX/XX/XXXX</td>
+                        <td>XXXXXXXXXX</td>
+                        <td><span>Aprovado</span></td>
+                        <td>
+                            <form>
+                                <button class="icon-btn">
+                                    <img class="icon" src="eye.png" alt="icone editar">
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
