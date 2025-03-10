9ab108bbafb3fbc1e0b9ec34b32578c834af3587
Allan Fernandes <allandossantosfernandes@gmail.com>
2025-03-10 19:58:17 -0400

    Resolvendo merge

--- view/pages/Adm/validarAtualizacaoOng.php
+++ view/pages/Adm/validarAtualizacaoOng.php
@@ -1,13 +1,11 @@
 <?php
-<<<<<<< HEAD
-=======
 // include __DIR__ . "\..\..\components\head.php";
->>>>>>> e77b384 (Iniciando atividade em php)
-require_once "../../../view/components/head.php";
+
+
 
 class DadosTabela
 {
-<<<<<<< HEAD
+
     private $dados = [
         ["Data" => '10/10/2024', "NomeOngs" => 'Esperança', "Status" => 'Aprovado'],
         ["Data" => '05/09/2024', "NomeOngs" => 'Vida', "Status" => 'Recusado'],
@@ -61,86 +59,9 @@ $arrays = $tabela->getDados();
                                 </td>
                             </tr>
                         <?php } ?>
-=======
-    private $Data;
-    private $NomeOngs;
-    private $Status;
-}
-
-$arrays = [
-    [$Data => 'XX/XX/XXXX'],
-    [$NomeOngs => 'XXXXXXXXXXXXX'],
-    [$Status => 'Aprovado'],
-];
-
-?>
-
-
-<body>
-    <?php
-    require_once "../../../view/components/navbar.php";
-    ?>
-    <main>
-        <section>
-            <div class="titulo">
-                <h1><?php echo "Cadastrais"; ?></h1>
-            </div>
-            <div>
-                <table class="table-validar">
-                    <thead>
-                        <tr class="row-head">
-                            <td>Data</td>
-                            <td>Nome das Ongs</td>
-                            <td>Status</td>
-                            <td>Visualizar</td>
-                        </tr>
-                    </thead>
-                    <tbody class="corpo-tabela">
-                        <?php
-                        foreach ($arrays as $array) { ?>
-                            <? php ?>
-                            <tr class="row-body">
-                                <?php
-                                echo "<td>$Data</td>";
-                                echo "<td>$Ongs</td>";
-                                echo "<td class='status-aguardando'>$Status</td>";
-                                echo "<td><span class='material-symbols-outlined'> visibility </span></td>";
-                        }
-                        ?>
-                        </tr>
-                        <tr class="row-body">
-                            <td>XX/XX/XXXX</td>
-                            <td>XXXXXXXXXXXXX</td>
-                            <td class="status-recusado">Recusado</td>
-                            <td>
-                                <span class="material-symbols-outlined"> visibility </span>
-                            </td>
-                        </tr>
-                        <tr class="row-body">
-                            <td>XX/XX/XXXX</td>
-                            <td>XXXXXXXXXXXXX</td>
-                            <td class="status-aprovado">Aprovado</td>
-                            <td>
-                                <span class="material-symbols-outlined"> visibility </span>
-                            </td>
-                        </tr>
-                        <tr class="row-body">
-                            <td>XX/XX/XXXX</td>
-                            <td>XXXXXXXXXXXXX</td>
-                            <td class="status-aguardando">Aguardando</td>
-                            <td>
-                                <span class="material-symbols-outlined"> visibility </span>
-                            </td>
-                        </tr>
->>>>>>> e77b384 (Iniciando atividade em php)
                     </tbody>
                 </table>
             </div>
         </section>
     </main>
-<<<<<<< HEAD
-    <script src="../../../view/assests/js/pages/verOngUsuario.js" defer></script>
-=======
-    <script src="script.js" defer></script>
->>>>>>> e77b384 (Iniciando atividade em php)
 </body>
