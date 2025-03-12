ea41c1fd8a675c2e5dcc70402c279f38f3a6c62d
Murilo460 <murilorocha.007@gmail.com>
2025-03-11 21:45:59 -0400

    up

--- view/pages/Usuario/loginUsuario.php
+++ view/pages/Usuario/loginUsuario.php
@@ -78,66 +78,6 @@
 
     </div><!--container-->
 
-    <script>
-        document.getElementById('criar-conta-button').addEventListener('click', function (event) {
-            event.preventDefault(); // Previne o comportamento padrão do botão
-    
-            const loginContent = document.getElementById('loginContent');
-            const recuperarContent = document.getElementById('recuperarContent');
-            const registrarContent = document.getElementById('registrarContent');
-    
-            loginContent.classList.remove('show');
-            loginContent.classList.add('hidden');
-            recuperarContent.classList.add('hidden');
-            registrarContent.classList.remove('hidden');
-            registrarContent.classList.add('show');
-    
-            setTimeout(() => {
-                registrarContent.style.right = '50px';
-            }, 10); 
-        });
-
-        document.getElementById('esqueci-senha-button').addEventListener('click', function (event) {
-            event.preventDefault(); // Previne o comportamento padrão do botão
-    
-            const loginContent = document.getElementById('loginContent');
-            const recuperarContent = document.getElementById('recuperarContent');
-            const registrarContent = document.getElementById('registrarContent');
-    
-            loginContent.classList.remove('show');
-            loginContent.classList.add('hidden');
-            recuperarContent.classList.add('show');
-            recuperarContent.classList.remove('hidden');
-            registrarContent.classList.add('hidden');
-            registrarContent.classList.remove('show');
-    
-            setTimeout(() => {
-                registrarContent.style.right = '50px'; 
-            }, 10); 
-        });
-
-
-        document.getElementById('house-icon-login-usuario').addEventListener('click', function (event) {
-            event.preventDefault(); // Previne o comportamento padrão do botão
-    
-            const loginContent = document.getElementById('loginContent');
-            const recuperarContent = document.getElementById('recuperarContent');
-            const registrarContent = document.getElementById('registrarContent');
-    
-            loginContent.classList.add('show');
-            loginContent.classList.remove('hidden');
-            recuperarContent.classList.add('hidden');
-            recuperarContent.classList.remove('show');
-            registrarContent.classList.add('hidden');
-            registrarContent.classList.remove('show');
-    
-            setTimeout(() => {
-                registrarContent.style.right = '50px'; 
-            }, 10); 
-        });
-    </script>
-
-
 </body>
 
 <?php require_once "../../../view/components/footer.php"?>
