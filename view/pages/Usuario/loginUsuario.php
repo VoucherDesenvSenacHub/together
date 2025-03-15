b903a73c396ab0e91e8682d366d8dcf2dc02088e
Murilo460 <murilorocha.007@gmail.com>
2025-03-14 21:17:24 -0400

    arrumei os id's

--- view/pages/Usuario/loginUsuario.php
+++ view/pages/Usuario/loginUsuario.php
@@ -20,7 +20,7 @@
                     <img src="../../assests/images/Usuario/usuario-user-foto.png" alt="foto-user" class="foto-perfil-login-usuario">
                 
                     <input type="text" placeholder="Digite seu Login" class="text-input-login-usuario" id="login-input">
-                    <input type="text" placeholder="Digite sua Senha" class="text-input-login-usuario" id="senha-input">
+                    <input type="password" placeholder="Digite sua Senha" class="text-input-login-usuario" id="senha-input">
 
                     <a href="#">
                         <p id="esqueci-senha-button" class="esqueci-senha-login-usuario">Esqueci a senha</p>
@@ -40,10 +40,10 @@
             <div class="box-login-usuario">
 
                 <form class="login-usuario">
-                    <button id="house-button-login-usuario"><i class="fa-solid fa-house fa-xl" id="house-icon-login-usuario"></i></button>
+                    <button id="house-button-recuperar-usuario"><i class="fa-solid fa-house fa-xl" id="house-icon-recuperar-usuario"></i></button>
                     <h1 class="titulo-login-usuario">Recuperar Senha</h1>
                     <p class="sub-titulo-login-usuario">Enviaremos um E-mail para você confirmar sua nova senha</p>
-                    <input type="text" placeholder="Email" class="text-input-login-usuario" id="email-input">
+                    <input type="text" placeholder="Email" class="text-input-login-usuario" id="email-esqueci-senha-input">
                     <button class="botao-login-usuario">Enviar</button>
                 </form>
 
@@ -56,12 +56,12 @@
         <div class="registrar-conta-login-usuario hidden" id="registrarContent">
             <div class="box-login-usuario">
                 <form class="registrar-box-login-usuario">
-                    <button id="house-button-login-usuario"><i class="fa-solid fa-house fa-xl" id="house-icon-login-usuario"></i></button>
+                    <button id="house-button-registrar-usuario"><i class="fa-solid fa-house fa-xl" id="house-icon-registrar-usuario"></i></button>
                     <input type="text" placeholder="Digite seu Nome" class="register-input-login-usuario" id="nome-input">
                     <input type="text" placeholder="Digite seu CPF" class="register-input-login-usuario" id="cpf-input">
                     <input type="text" placeholder="Digite seu Telefone" class="register-input-login-usuario" id="telefone-input">
                     <input type="email" placeholder="Digite seu Email" class="register-input-login-usuario" id="email-input">
-                    <input type="password" placeholder="Digite sua Senha" class="register-input-login-usuario" id="senha-input">
+                    <input type="password" placeholder="Digite sua Senha" class="register-input-login-usuario" id="senha-registrar-input">
                     <input type="password" placeholder="Confirme a Senha" class="register-input-login-usuario" id="confirme-senha-input">
                     <div class="termos-area-login-usuario">
                         <input type="checkbox" id="checkbox">
