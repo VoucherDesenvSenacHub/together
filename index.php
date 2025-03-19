8bfdbffcfa350e2cfd27e46d7200b7e552274f9c
Rhyan Santiago Komm <rhyankomm@hotmail.com.com>
2025-03-18 21:14:43 -0400

    att

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
         <div class="home-banner-together"></div>
         <div class="home-card-box">
             <h3 class="home-title-favorite">Destaques</h3>
+            <div class="home-div-botao">
             <button class="botao">Ver Mais</button>
+            </div>
             <div class="home-scroll-area">
                 <?php for ($i = 0; $i < 5; $i++) { ?>
                     <article class="card-container">
