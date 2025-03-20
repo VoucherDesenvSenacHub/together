f29430be151744cdabbcfb26628166bb85cc86ad
Rogerio Laporta <rogerio54177286@aluno.ms.senac.br>
2025-03-19 21:43:11 -0400

    iniciando a task

--- view/pages/Ong/validacaoOng.php
+++ view/pages/Ong/validacaoOng.php
@@ -1,4 +1,5 @@
-<?php require_once "./../../components/head.php"; ?>
+<?php require_once "./../../components/head.php";
+ ?>
 
 <body>
     <header>
@@ -6,48 +7,50 @@
     </header>
     <main class="main-container">
         <!-- Começar aqui -->
-        <table class="validacao-voluntario-tabela">
+        <?php require_once "./../../components/back-button.php" ?>
+        <h1 class="validacao-ong-h1" >Validação de ONGs</h1>
+        <table class="validacao-ong-tabela">
                 <thead>
                     <tr>
                         <th>Data</th>
-                        <th>Voluntário</th>
+                        <th>Nome das ONGs</th>
                         <th>Status</th>
                         <th>Visualizar</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="validacao-voluntario-texto">12/03/2024</td>
-                        <td class="validacao-voluntario-texto">João Silva</td>
-                        <td class="validacao-voluntario-texto">Aguardando</td>
+                        <td class="validacao-ong-texto">12/03/2024</td>
+                        <td class="validacao-ong-texto">João Silva</td>
+                        <td class="validacao-ong-texto">Aguardando</td>
                         <td>
-                            <button class="validacao-voluntario-botao-visualizar">
+                            <button class="validacao-ong-botao-visualizar">
                                 <span class="material-symbols-outlined">
-                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-voluntario-icone-visualizar"></i></a>
+                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-ong-icone-visualizar"></i></a>
                                 </span>
                             </button>
                         </td>
                     </tr>
                     <tr>
-                        <td class="validacao-voluntario-texto">10/03/2024</td>
-                        <td class="validacao-voluntario-texto">Maria Oliveira</td>
-                        <td class="validacao-voluntario-texto">Aguardando</td>
+                        <td class="validacao-ong-texto">10/03/2024</td>
+                        <td class="validacao-ong-texto">Maria Oliveira</td>
+                        <td class="validacao-ong-texto">Aguardando</td>
                         <td>
-                            <button class="validacao-voluntario-botao-visualizar">
+                            <button class="validacao-ong-botao-visualizar">
                                 <span class="material-symbols-outlined">
-                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-voluntario-icone-visualizar"></i></a>
+                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-ong-icone-visualizar"></i></a>
                                 </span>
                             </button>
                         </td>
                     </tr>
                     <tr>
-                        <td class="validacao-voluntario-texto">08/03/2024</td>
-                        <td class="validacao-voluntario-texto">Carlos Souza</td>
-                        <td class="validacao-voluntario-texto">Aguardando</td>
+                        <td class="validacao-ong-texto">08/03/2024</td>
+                        <td class="validacao-ong-texto">Carlos Souza</td>
+                        <td class="validacao-ong-texto">Aguardando</td>
                         <td>
-                            <button class="validacao-voluntario-botao-visualizar">
+                            <button class="validacao-ong-botao-visualizar">
                                 <span class="material-symbols-outlined">
-                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-voluntario-icone-visualizar"></i></a>
+                                    <a href="https://google.com"><i class="fa-regular fa-eye validacao-ong-icone-visualizar"></i></a>
                                 </span>
                             </button>
                         </td>
