dd48b5d33f45c46a1b490ce1966dd5ded1c7544f
Murilo460 <murilorocha.007@gmail.com>
2025-03-19 21:01:55 -0400

    up

---
+++ view/pages/Adm/visaoDoUsuario.php
@@ -0,0 +1,34 @@
+<?php require_once "../../../view/components/head.php" ?>
+
+<body class="body-visao-usuario">
+    <?php require_once "../../../view/components/navbar.php"; ?>
+
+    <main class="main-container">
+
+        <div class="container-visao-usuario">
+
+            <div class="image-area-visao-usuario">
+                <img class="image-usuario-visao-usuario" src="../../../view/assests/images/components/usuario-user-foto.png" alt="foto-usuario">
+                <button class="botao-inativar-visao-usuario">
+                    Inativar
+                    <i class="fa-solid fa-trash" id="trash-icon-visao-usuario"></i>
+                </button>
+            </div>
+
+            <div class="info-area-visao-usuario">
+
+                <!-- Tudo dentro de um Form e uma div com label e input separadas 
+                    Ex: <div>
+                            <label>
+                            <input>
+                        </div>-->
+
+            </div>
+
+
+            <div class="confirmacao-area-visao-usuario"></div>
+            
+        </div>
+
+    </main>
+</body>
