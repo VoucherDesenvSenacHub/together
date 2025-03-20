74e46d03013bc3cdc52fd128a844a74719e98db3
Rodrigo Ramalho <rodrigo51685776@aluno.ms.senac.br>
2025-03-19 20:01:50 -0400

    Estruturando a task

---
+++ view/pages/Ong/validacaoOng.php
@@ -0,0 +1,10 @@
+<?php require_once "./../../components/head.php"; ?>
+
+<body>
+    <header>
+        <?php require_once "./../../components/navbar.php"; ?>
+    </header>
+    <main class="main-container">
+        <!-- Começar aqui -->
+    </main>    
+</body>
