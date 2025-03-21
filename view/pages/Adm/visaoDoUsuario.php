8aa21ca55a3e4f5410eccab7e77d2a6a05167145
Fernanda Souza <fernanda54099666@aluno.ms.senac.br>
2025-03-20 21:43:16 -0400

    fiz o forms html

--- view/pages/Adm/visaoDoUsuario.php
+++ view/pages/Adm/visaoDoUsuario.php
@@ -15,44 +15,61 @@
                 </button>
             </div>
 
+
             <div class="info-area-visao-usuario">
-                <div class="nome-teste">
-                    <form action="">
 
-                        <label for="">Nome</label>
-                        <input type="name" >
+                <form action="#" method="post" class="form-area-visao-do-usuario">
+
+                    <div class="form-visao-do-usuario">
+                        <label for="nome" class="descricao">Nome</label>
+                        <input class="input-visao-do-usuario" type="text" id="nome" name="nome" autocomplete="off" required
+                        placeholder="xxxxxxxxxxxxx"  >
                         
+                    </div>
 
-                    </form>
-                </div>
+                    <div class="form-visao-do-usuario">
+                        <label for="cpf" class="descricao">CPF</label>
+                        <input class="input-visao-do-usuario" type="text" id="cpf" name="cpf" autocomplete="off" required placeholder="xxx.xxx.xxx-xx">
+                        <i class="fa-solid fa-file-lines"></i>
+                    </div>
 
-                <div class="nome-teste">
-                    <form action="">
 
-                        <label for="">CPF</label>
-                        <input type="cpf" name="cpf" >
-                        
+                    <div class="divisao-visao-do-usuario"> <!--Comeco da parte do nasc e tel -->
 
-                    </form>
-                </div>
-                
-                <div class="nome-teste">
-                    <form action="">
+                        <div class="data-nasc-visao-do-usuario">
+                            <label for="data" class="descricao">Data de Nascimento</label>
+                            <input class="input-divisao" type="date" id="data" name="data" autocomplete="off" required placeholder="xx/xx/xxxx">
+                            <i class="fa-solid fa-cake-candles"></i>
+
+                        </div>
                     
-                        <label for="">CPF</label>
-                        <input type="cpf" name="cpf" >
-                        
 
-                    </form>
-                </div>
+                        <div class="tel-visao-do-usuario">
+                            <label for="telefone" class="descricao">Telefone</label>
+                            <input class="input-divisao" type="tel" id="telefone" name="telefone" autocomplete="off" required placeholder="+55(xx) xxxx-xxxx">
+                            <i class="fa-solid fa-phone"></i>
+
+                        </div>
+
+                    </div> <!--término da parte do nasc e tel -->
+
+                    <div class="form-visao-do-usuario">
+                        <label for="endereco" class="descricao">Endereço</label>
+                        <input class="input-visao-do-usuario" type="text" id="endereco" name="endereco" autocomplete="off" required placeholder="xxxxxxxxxx">
+                        <i class="fa-solid fa-location-dot"></i>
+
+                    </div>
+
+                    <div class="form-visao-do-usuario">
+                        <label for="email" class="descricao">Email</label>
+                        <input class="input-visao-do-usuario" type="email" id="email" name="email" autocomplete="off" required placeholder="xxxxxxxxxxx">
+                        <i class="fa-solid fa-envelope"></i>
+
+                    </div>
 
 
-
-                <!-- Tudo dentro de um Form e uma div com label e input separadas 
-                    Ex: <div>
-                            <label>
-                            <input>
-                        </div>-->
+                    
+                </form>
 
             </div>
 
@@ -63,3 +80,9 @@
 
     </main>
 </body>
+
+<!-- Tudo dentro de um Form e uma div com label e input separadas 
+                    Ex: <div>
+                            <label>
+                            <input>
+                        </div>-->
