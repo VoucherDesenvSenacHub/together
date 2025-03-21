90cafcc5157dae405be71000824e1d54e674437b
AntonioV1ctor <antoniovictor.me@gmail.com>
2025-03-20 21:53:54 -0400

    Add new colors and class

--- view/pages/Adm/visaoSobreaOng.php
+++ view/pages/Adm/visaoSobreaOng.php
@@ -9,7 +9,8 @@ require_once "../../../view/components/head.php";
             <form method="post" action="#" class="adm-ong-vision-form-box">
                 <div class="adm-ong-vision-area-limiter">
                     <div class="adm-ong-vision-filter-tags">
-                        <h3>Fome Zero e Agricultura Sustentavel</h3>
+                        <i id="adm-ong-vision-icon-default" class="fa-solid fa-tag fa-rotate-90"></i>
+                        <h3 id="adm-ong-vision-filter-tag-title" class="adm-ong-vision-default-text">Fome Zero e Agricultura Sustentavel</h3>
                     </div>
                     <div class="adm-ong-vision-title-options">
 
@@ -18,35 +19,36 @@ require_once "../../../view/components/head.php";
                         </div>
                         <div class="adm-ong-vision-title-div">
                             <strong class="adm-ong-vision-title">Associação Prato Cheio</strong>
-                            <div>
-                                <p>A Associação Prato Cheio combate a fome de pessoas em situação de vulnerabilidade social e promove sistemas alimentares sustentáveis.</p>
+                            <div class="adm-ong-vision-subtitle">
+                                <p class="adm-ong-vision-default-text">A Associação Prato Cheio combate a fome de pessoas em situação de vulnerabilidade social e promove sistemas alimentares sustentáveis.</p>
+                            </div>
+                            <div class="adm-ong-vision-button-div">
+                                <input class="adm-ong-vision-donate-button" type="button" value="Fazer Doação">
+                                <input class="adm-ong-vision-volunter-button" type="button" value="Voluntariar-se">
                             </div>
                             <div>
-                                <input type="button" value="Fazer Doação">
-                                <input type="button" value="Voluntariar-se">
-                            </div>
-                            <div>
-                                <p>* Sua doação será feita diretamente para o Instituto Benfeitoria, que irá repassar os valores às organizações beneficiadas.</p>
+                                <p class="adm-ong-vision-default-text">* Sua doação será feita diretamente para o Instituto Benfeitoria, que irá repassar os valores às organizações beneficiadas.</p>
                             </div>
                         </div>
                     </div>
-                    <div class="adm-ong-vision-about-location">
-                        <h3>Campo Grande - MS R. Jardim Botânico 288</h3>
+                    <div class="adm-ong-vision-about-location-div">
+                        <i id="adm-ong-vision-icon-default" class="fa-solid fa-location-dot"></i>
+                        <h3 id="adm-ong-vision-about-location-title" class="adm-ong-vision-default-text">Campo Grande - MS R. Jardim Botânico 288</h3>
                     </div>
                     <div class="adm-ong-vision-about-div">
-                        <p>
+                        <p class="adm-ong-vision-default-text">
                             O trabalho da Associação Prato Cheio é voltado para atender pessoas em situação de vulnerabilidade social em São Paulo e região. A Prato Cheio acredita que a a redução das Perdas e Desperdício de Alimentos é essencial para combater a fome e a desnutrição e ao mesmo tempo ajudar a promover sistemas alimentares sustentáveis.
                         </p>
 
-                        <p>
+                        <p class="adm-ong-vision-default-text">
                             Em 21 anos de trabalho, já foram coletados e doados 4 milhões de kg de alimentos, que teriam sido desperdiçados, e que melhoram a alimentação de 25 mil pessoas diretamente. Em 1 dia de trabalho, a Prato Cheio coleta em média 2 mil kg de alimentos, equivalente a 4 mil refeições por dia - e tudo feito de forma gratuita! Menos desperdício de alimentos, mais saúde e sustentabilidade.
                         </p>
 
-                        <p>
+                        <p class="adm-ong-vision-default-text">
                             A Associação Prato Cheio atua em dois temas: redução da fome e desnutrição e redução do desperdício de alimentos.
                         </p>
 
-                        <p>
+                        <p class="adm-ong-vision-default-text">
                             A Prato Cheio desenvolve o Projeto Rota Solidária, coletando alimentos frescos que perderam valor comercial em diferentes locais (supermercados, indústrias, produtores), mas que mantém suas propriedades nutricionais preservadas, e doando para instituições sociais e no Projeto Oficinas Solidárias, oferencendo cursos e oficinas gratuitas sobre técnicas e receitas para redução do desperdício.
                         </p>
                     </div>
