90b587bc67157319c8e1676f7f3f714972f36209
Rogério Vicente de Carvalho Laporta <rogervclaporta@gmail.com>
2025-04-08 21:45:32 -0400

    comecando a tela

--- view/pages/Ong/editarPerfilOng.php
+++ view/pages/Ong/editarPerfilOng.php
@@ -7,4 +7,20 @@
         <?php require_once './../../components/back-button.php'?>
         
     </main>
+    <div class="container-titulo">
+        <label class="titulo-label" for="">Titulo</label> 
+        <input class="entrada-titulo" type="text" placeholder=""> 
+    </div>
+
+    <div class="container-subtitulo" >
+        <label for="">Subtitulo</label>
+        <input  class="entrada-subtitulo" type="text" placeholder="">
+    </div>
+
+    <div class="container-descricao" >
+        <label for="">Descrição</label>
+        <input  class="entrada-descricao" type="text" placeholder="">
+    </div>
+
+
 </body>
