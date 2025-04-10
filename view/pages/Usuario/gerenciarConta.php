90df5b68432e9117db2d2e16f372c0ddcfeac39d
Henrico Queiroz <Henricooq@gmail.com>
2025-04-09 21:48:00 -0400

    adição da página

---
+++ view/pages/Usuario/gerenciarConta.php
@@ -0,0 +1,56 @@
+<?php require_once "../../../view/components/head.php"?>
+  <body>
+  <?php require_once './../../components/navbar.php' ?>
+    <section class="Reset">
+      <!-- <div class="gerenciar-conta
+        <a href="#" class="button-perfil">
+          <img
+            class="imagem"
+            src="/together/view/assests/images/Usuario/usuario-user-foto.png"
+            alt="foto Usuario"
+          />
+          <p>Editar Usuario</p>
+          <span class="material-symbols-outlined"> arrow_forward_ios </span>
+        </a>
+      </div> -->
+      <div class="main">
+        <div class="leftSide">
+          <a href="#" class="conjunto_botoes">
+            <p class="format_p">Ongs em que eu sou voluntário</p>
+            <span class="material-symbols-outlined"> chevron_right </span>
+            <span id="icone_2" class="material-symbols-outlined">
+              handshake
+            </span>
+          </a>
+          <a href="#" class="conjunto_botoes">
+            <span id="icone_2" class="material-symbols-outlined"> favorite </span>
+            <p class="format_p">Favoritos</p>
+            <span class="material-symbols-outlined"> chevron_right </span>
+          </a>
+          <a href="#" class="conjunto_botoes">
+            <p class="format_p">Historico de doação</p>
+            <span id="icone_2" class="material-symbols-outlined"> schedule </span>
+            <span class="material-symbols-outlined"> chevron_right </span>
+          </a>
+        </div>
+        
+        <hr />
+        
+        <div class="dados_usuario">
+          <img
+          class="foto_usuario"
+          src="/together/view/assests/images/Usuario/usuario-user-foto.png"
+          alt="foto Usuario"
+          />
+          <div class="dados">
+            <h4>Sua localização</h4>
+            <h3><strong>Nome do Usuario</strong></h3>
+            <h3><strong>Telefone do Usuario</strong></h3>
+            <h3><strong>Email do Usuario</strong></h3>
+            <h3><strong>Cpf do Usuario</strong></h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
