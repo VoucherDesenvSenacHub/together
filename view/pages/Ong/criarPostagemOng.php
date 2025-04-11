162ae1686cafbef1fae6657476b24ff8c9f4d701
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-04-10 21:48:15 -0400

    a tela só esta faltando a responsividade e o java script

--- view/pages/Ong/criarPostagemOng.php
+++ view/pages/Ong/criarPostagemOng.php
@@ -1,46 +1,35 @@
 <?php require_once "../../../view/components/head.php"; ?>
+<?php require_once "../../components/button.php" ?>
 
 <body>
     <?php require_once "../../../view/components/navbar.php"; ?>
-    <main class="main-container">
-        <?php require_once './../../components/back-button.php'?>
-        <form action="" class="postagem-ong-geral-form" id="postagem-ong-geral-form">
-            <div class="postagem-ong-geral-div-group">
-                <div class="postagem-ong-geral-input-group">
-                    <h2 class="postagem-ong-geral-titulo-postagem">Imagem</h2>
-                    <ul id="postagem-ong-geral-images"></ul>
-                    <input class="postagem-ong-geral-input-file" id="postagem-ong-geral-file-input" type="file" accept="image/*">
-                    <label class="postagem-ong-geral-label-img" for="postagem-ong-geral-file-input">
-                        <img class="postagem-ong-geral-label-icon" src="../../assests/images/ong/add.png" alt="icon add">
-                    </label>
-                </div>
-            </div>
-            <div class="postagem-ong-geral-div-group">
-                <div class="postagem-ong-geral-input-group">
-                    <h2 class="postagem-ong-geral-titulo-postagem">Título</h2>
-                    <div class="postagem-ong-geral-input-titulo">
-                        <input class="postagem-ong-geral-input-postagem" id="postagem-ong-geral-hyperlink" type="url" required >
-                    </div>
-                </div>
-                <div class="postagem-ong-geral-input-group">
-                    <h2 class="postagem-ong-geral-titulo-postagem">Descrição</h2>
-                    <div class="postagem-ong-geral-input-descricao">
-                        <textarea class="postagem-ong-geral-textarea-postagem" id="postagem-ong-geral-texto" type="text" maxlength="255" required></textarea>
-                    </div>
-                </div>
-                <div class="postagem-ong-geral-input-group">
-                    <h2 class="postagem-ong-geral-titulo-postagem">Hiperlink</h2>
-                    <input class="postagem-ong-geral-input-postagem" id="postagem-ong-geral-hyperlink" type="url" required >
-                </div>
-            </div>
-            <div class="postagem-ong-geral-btn-div">
-                <button class="botao salvar" type="submit">Salvar</button>
-                <button class="botao cancelar" type="reset">Cancelar</button>
-            </div>
-        </form>
-    </main>
-    <?php require_once "../../../view/components/footer.php"; ?>
 
-    <!-- Inclua o arquivo JavaScript externo -->
+    <main class="main-container">
+        <?php require_once './../../components/back-button.php' ?>
+
+        <div class="formulario-perfil">
+            <form action="">
+                <div class="formulario-linha-superior">
+                    <?php require_once "./../../components/upload.php" ?>
+                    <div class="formulario-campos">
+                        <label class="formulario-label" for="titulo">Título</label>
+                        <input class="formulario-input" type="text" id="titulo" required/>
+
+                        <label class="formulario-label" for="link">Link</label>
+                        <input class="formulario-input" id="link" required>
+                    </div>
+                </div>
+
+                <label class="formulario-label" for="descricao">Descrição</label>
+                <textarea class="formulario-textarea" id="descricao" rows="4"></textarea>
+
+                <div class="postagem-geral-btn-group">
+                    <?= botao('primary', 'Salvar') ?>
+                    <?= botao('secondary', 'Cancelar') ?>
+                </div>
+            </form>
+        </div>
+    </main>
+
+
 </body>
-</html>
--- view/pages/Ong/editarPostagemOng.php
+++ view/pages/Ong/editarPostagemOng.php
@@ -1,4 +1,5 @@
 <?php require_once "../../../view/components/head.php"; ?>
+<?php require_once "../../components/button.php" ?>
 
 <body>
     <?php require_once "../../../view/components/navbar.php"; ?>
@@ -9,27 +10,31 @@
         <div class="formulario-perfil">
             <form action="">
                 <div class="formulario-linha-superior">
-                    <?php require_once "./../../components/upload.php"?>
+                    <?php require_once "./../../components/upload.php" ?>
                     <div class="formulario-campos">
-                        <label class="form-label" for="titulo">Título</label>
-                        <input class="form-input" type="text" id="titulo" />
+                        <label class="formulario-label" for="titulo">Título</label>
+                        <input class="formulario-input" type="text" id="titulo" />
 
-                        <label class="form-label" for="link">Link</label>
-                        <input class="form-input" id="link"></input>
+                        <label class="formulario-label" for="link">Link</label>
+                        <input class="formulario-input" id="link"></input>
                     </div>
                 </div>
 
-                <label class="form-label" for="descricao">Descrição</label>
-                <textarea class="form-textarea" id="descricao" rows="4"></textarea>
+                <label class="formulario-label" for="descricao">Descrição</label>
+                <textarea class="formulario-textarea" id="descricao" rows="4"></textarea>
 
-                    <div class="formulario-buttons">
-                        <button class="botao salvar" type="submit">Salvar</button>
-                        <button class="botao cancelar" type="reset">Cancelar</button>
+                <div class="postagem-geral-div-group">
+                    <div class="postagem-geral-div-excluir">
+                        <img src="./../../assests/images/geral/lixeira.png" alt="icon excluir" class="postagem-geral-excluir">
+                    </div>
+                    <div class="postagem-geral-btn-group">
+                        <?= botao('primary', 'Salvar') ?>
+                        <?= botao('secondary', 'Cancelar') ?>
                     </div>
                 </div>
             </form>
         </div>
     </main>
-    
+
 
 </body>
