1a6211dde0f12985cef8afb76e0214de85f04d55
vitorgalvao0 <vitoribeirogr@gmail.com>
2025-04-11 21:37:21 -0400

    andamento...faltando ultimos detalhes

--- view/pages/Ong/relatorioOng.php
+++ view/pages/Ong/relatorioOng.php
@@ -10,63 +10,63 @@
                     <div class="relatorio-ong-title-div">
                         <h1 class="h1-relatorio-ong">Relatórios</h1>
                     </div>
-                        <div class="relatorio-ong-statistic-cards-div">
-                            <div class="relatorio-ong-card-clicks-1">
-                                <div title="+1,0 mil Clicks" class="relatorio-ong-card-text-value">
-                                    <h3 class="h3-relatorio-ong">Clicks para Doação</h3>
-                                    <h2 class="h2-relatorio-ong">2,2 mil</h2>
-                                    <div class="relatorio-ong-statistic-green-text">
-                                        <i class="fa-solid fa-arrow-up"></i>
-                                        <p class="p-relatorio-ong">1,0 mil em Junho</p>
-                                    </div>
-                                </div>
-                                <div class="relatorio-ong-card-icon-div">
-                                    <i class="fa-solid fa-circle-plus relatorio-ong-icon-card-1"></i>
+                    <div class="relatorio-ong-statistic-cards-div">
+                        <div class="relatorio-ong-card-clicks-1">
+                            <div title="+1,0 mil Clicks" class="relatorio-ong-card-text-value">
+                                <h3 class="h3-relatorio-ong">Clicks para Doação</h3>
+                                <h2 class="h2-relatorio-ong">2,2 mil</h2>
+                                <div class="relatorio-ong-statistic-green-text">
+                                    <i class="fa-solid fa-arrow-up"></i>
+                                    <p class="p-relatorio-ong">1,0 mil em Junho</p>
                                 </div>
                             </div>
-                            <div class="relatorio-ong-card-acess-2">
-                                <div title="+1,0 mil Acessos" class="relatorio-ong-card-text-value">
-                                    <h3 class="h3-relatorio-ong">Acessos na Página</h3>
-                                    <h2 class="h2-relatorio-ong">1,6 mil</h2>
-                                    <div class="relatorio-ong-statistic-green-text">
-                                        <i class="fa-solid fa-arrow-up"></i>
-                                        <p class="p-relatorio-ong">1,0 mil em Junho</p>
-                                    </div>
-                                </div>
-                                <div class="relatorio-ong-card-icon-div">
-                                    <i class="fa-regular fa-message relatorio-ong-icon-card-2"></i>
-                                </div>
-                            </div>
-                            <div class="relatorio-ong-card-volunters-3">
-                                <div title="+100 Voluntários" class="relatorio-ong-card-text-value">
-                                    <h3 class="h3-relatorio-ong">Adesão de Voluntários</h3>
-                                    <h2 class="h2-relatorio-ong">215,0</h2>
-                                    <div class="relatorio-ong-statistic-green-text">
-                                        <i class="fa-solid fa-arrow-up"></i>
-                                        <p class="p-relatorio-ong">100 em Junho</p>
-                                    </div>
-                                </div>
-                                <div class="relatorio-ong-card-icon-div">
-                                    <i class="fa-solid fa-user relatorio-ong-icon-card-3"></i>
-                                </div>
+                            <div class="relatorio-ong-card-icon-div">
+                                <i class="fa-solid fa-circle-plus relatorio-ong-icon-card-1"></i>
                             </div>
                         </div>
-                        <div class="relatorio-ong-ong-options-4">
-                            <a title="Visualizar Doadores" class="relatorio-ong-default-icon-div" href="#">
-                                <i id="relatorio-ong-yey-icon" class="fa-solid fa-eye"></i>
-                                <p>Visualizar Doadores </p>
-                            </a>
-                            <a title="Baixar Relatório" class="relatorio-ong-default-icon-div" href="#">
-                                <i id="relatorio-ong-yey-icon" class="fa-solid fa-download"></i>
-                                <p>Baixar Relatório</p>
-                            </a>
+                        <div class="relatorio-ong-card-acess-2">
+                            <div title="+1,0 mil Acessos" class="relatorio-ong-card-text-value">
+                                <h3 class="h3-relatorio-ong">Acessos na Página</h3>
+                                <h2 class="h2-relatorio-ong">1,6 mil</h2>
+                                <div class="relatorio-ong-statistic-green-text">
+                                    <i class="fa-solid fa-arrow-up"></i>
+                                    <p class="p-relatorio-ong">1,0 mil em Junho</p>
+                                </div>
+                            </div>
+                            <div class="relatorio-ong-card-icon-div">
+                                <i class="fa-regular fa-message relatorio-ong-icon-card-2"></i>
+                            </div>
+                        </div>
+                        <div class="relatorio-ong-card-volunters-3">
+                            <div title="+100 Voluntários" class="relatorio-ong-card-text-value">
+                                <h3 class="h3-relatorio-ong">Adesão de Voluntários</h3>
+                                <h2 class="h2-relatorio-ong">215,0</h2>
+                                <div class="relatorio-ong-statistic-green-text">
+                                    <i class="fa-solid fa-arrow-up"></i>
+                                    <p class="p-relatorio-ong">100 em Junho</p>
+                                </div>
+                            </div>
+                            <div class="relatorio-ong-card-icon-div">
+                                <i class="fa-solid fa-user relatorio-ong-icon-card-3"></i>
+                            </div>
                         </div>
                     </div>
-                    <div title="Gráfico do projeto" class="relatorio-ong-graphic-div">
-                        <h1>Acesso á pagina da ONG</h1>
+                    <div class="relatorio-ong-ong-options-4">
+                        <a title="Visualizar Doadores" class="relatorio-ong-default-icon-div" href="#">
+                            <i id="relatorio-ong-yey-icon" class="fa-solid fa-eye"></i>
+                            <p>Visualizar Doadores </p>
+                        </a>
+                        <a title="Baixar Relatório" class="relatorio-ong-default-icon-div" href="#">
+                            <i id="relatorio-ong-yey-icon" class="fa-solid fa-download"></i>
+                            <p>Baixar Relatório</p>
+                        </a>
                     </div>
                 </div>
+                <div title="Gráfico do projeto" class="relatorio-ong-graphic-div">
+                    <h1>Acesso á pagina da ONG</h1>
+                </div>
             </div>
+        </div>
         </div>
     </main>
     <?php require_once "./../../components/footer.php"; ?>
