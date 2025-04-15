18b7cb8b71ede3d8e340d6e8fb34261949f6572f
Vitor-Novaga <vitor7633136@aluno.ms.senac.br>
2025-04-14 21:00:01 -0400

    Save

---
+++ view/pages/Usuario/pagamento_Usuario.php
@@ -0,0 +1,18 @@
+<?php require_once "../../../view/components/head.php";?>
+
+<body class="user_pay">
+    <?php require_once "../../../view/components/navbar.php";?>
+
+    <main class="main-container">
+    <?php require_once './../../components/back-button.php'?>
+    <div class="container-pagamento-usuarios">
+
+    </div>
+
+
+    </main>
+
+
+    <?php require_once "../../../view/components/footer.php"; ?>
+</body>
+</html>
