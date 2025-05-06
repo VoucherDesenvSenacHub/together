a23855e856690bcf150f3922f968700fb685a337
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-05-06 19:19:35 -0400

    Falta somente alguns detalhes

--- view/home.php
+++ view/home.php
@@ -1,3 +1,4 @@
+<?php require_once './../view/components/button.php' ?>
 <?php require_once './../view/components/head.php' ?>
 <?php require_once './../view/components/card.php' ?>
 <?php require_once './../view/components/button.php' ?>
@@ -13,7 +14,7 @@
                     <?= botao("primary","Ver Mais") ?>
                 </div>
                 <div class="card-container">
-                    <?php for ($i = 0; $i < 5; $i++) { ?>
+                    <?php for ($i = 0; $i < 4; $i++) { ?>
                         <?= cardOng("./../view\assests\images\Geral\ImageONG.png","Salva Vidas!","Salvamos a vida de animais abandonados, moradores de rua e todas as pessoas necessitadas.") ?>
                     <?php }
                     ; ?>
@@ -36,10 +37,6 @@
                             </p>
                             <hr>
                         </div>
-                    </div>
-                    <div class="sobre-nos-container">
-                        <img class="sobre-nos-image" src="/together/view/assests/images/Geral/img-sobre-nos.png"
-                            alt="Imagem sobre nós">
                     </div>
                 </div>
                 <div class="sobre-nos-cards-section">
