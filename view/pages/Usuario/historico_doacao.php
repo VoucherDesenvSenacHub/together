5f5d108b6e57a34280fae8b3547c6e505163748a
All4nFernandes <allandossantosfernandes@gmail.com>
2025-05-14 21:35:01 -0400

    Base da tela feita.

---
+++ view/pages/Usuario/historico_doacao.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once __DIR__ . '/../../components/head.php';
+
+?>
+
+
+<body>
+    <?php require_once __DIR__ . '/../../components/navbar.php'; ?>
+    <main class="main-container">
+        <h1 class="titulo-pagina-tabela">Historico</h1>
+
+        <table class="tabela">
+            <thead>
+                <tr>
+                    <th>
+
+                    </th>
+                    <th>
+
+                    </th>
+                    <th>
+
+                    </th>
+                    <th>
+
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                    <td>
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+    <?php require_once __DIR__ . '/../../components/footer.php'; ?>
+</body>
