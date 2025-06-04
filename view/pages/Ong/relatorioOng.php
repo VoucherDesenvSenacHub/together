cfe65529d6857f69e8611eb04959a29ff965b89a
rogeriovc <rogervclaporta@gmail.com>
2025-06-04 19:48:37 -0400

    atualizando a tela

--- view/pages/Ong/relatorioOng.php
+++ view/pages/Ong/relatorioOng.php
@@ -55,10 +55,6 @@
                         </div>
                     </div>
                     <div class="relatorio-ong-ong-options-4">
-                        <a title="Visualizar Doadores" class="relatorio-ong-default-icon-div" href="#">
-                            <i id="relatorio-ong-yey-icon" class="fa-solid fa-eye"></i>
-                            <p>Visualizar Doadores </p>
-                        </a>
                         <a title="Baixar Relatório" class="relatorio-ong-default-icon-div" href="#">
                             <i id="relatorio-ong-yey-icon" class="fa-solid fa-download"></i>
                             <p>Baixar Relatório</p>
@@ -70,20 +66,16 @@
                     <thead>
                         <tr>
                             <th>Data</th>
-                            <th>Nome das Ongs</th>
-                            <th>Status</th>
-                            <th>Visualizar</th>
+                            <th>Doador</th>
+                            <th>Valor</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php for ($i = 0; $i < 10; $i++): ?>
                             <tr>
                                 <td>xx/xx/xxxx</td>
-                                <td>Vida Pet</td>
-                                <td>Aguardando</td>
-                                <td>
-                                    <?= renderAcao('editar') ?>
-                                </td>
+                                <td>Lucas Andrade</td>
+                                <td>R$50,00</td>
                             </tr>
                         <?php endfor; ?>
                     </tbody>
