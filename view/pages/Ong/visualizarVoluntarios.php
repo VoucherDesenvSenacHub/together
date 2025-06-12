c3228a17cab825477a27510ac45ed041b5e08979
Vitor-Novaga <vitor7633136@aluno.ms.senac.br>
2025-06-11 21:44:43 -0400

    corrigindo

--- view/pages/Ong/visualizarVoluntarios.php
+++ view/pages/Ong/visualizarVoluntarios.php
@@ -3,6 +3,8 @@
 <?php require_once "../../../view/components/input.php"; ?>
 <?php require_once "../../../view/components/button.php"; ?>
 
+<!-- ESSA TELA VAI SER ALTERADA ELA VAI SER SOMENTE PARA ACEITAR O CADASTRO DA ONG -->
+
 <body>
     <?php require_once "../../../view/components/navbar.php"; ?>
     <main class="main-container">
@@ -10,113 +12,126 @@
             <?php require_once './../../components/back-button.php' ?>
         </div>
 
-        <div class="titulo-pagina-tabela">
-            <h1 class="titulo-pagina-tabela">Dados do voluntário</h1>
+        <div class="titulo-validar-atualizacao">
+            <h1 class="titulo-pagina-tabela">Validar Cadastro Ong</h1>
         </div>
 
         <div class="formulario-perfil">
-        <form action="" method="POST">
-    <div class="container-perfil-voluntario">
-        <img src="\together\view\assests\images\Ong\perfil-user.png" alt="Foto do usuário" class="logo-user">
-        
-        <div class="container-readonly">
-            <div class="container-readonly-primary">
+            <form action="" method="POST">
+                <div class="container-perfil-ong-atualizado">
+                    <img src="\together\view\assests\images\Ong\perfil-user.png" alt="Foto do usuário" class="logo-user">                    <div class="container-uper-readonly">
+                        <div class="container-uper-readonly-primary">
 
-                <div class="container-linha-campos">
-                    <div class="campo-formulario">
-                        <?= label('nome', 'Nome') ?>
-                        <?= inputReadonly('text', 'nome', 'nome', 'Jhon F. Kennedy') ?>
+                            <div class="form-row">
+
+                                <div>
+                                    <?= label('nome', 'Nome') ?> 
+                                    <?= inputReadonly('text', 'nome', 'nome', 'Jhon F. Kennedy') ?> 
+                                </div>
+
+                                <div>
+                                    <?= label('telefone', 'Telefone') ?> 
+                                    <?= inputReadonly('text', 'telefone', 'telefone', '+55 (67) 9 9999-9999') ?> 
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+
+                                <div>
+                                    <?= label('cpf', 'CPF') ?> 
+                                    <?= inputReadonly('text', 'cpf', 'cpf', '000.000.000-00') ?> 
+                                </div>
+
+                                <div>
+                                    <?= label('data', 'Nascimento') ?> 
+                                    <?= inputReadonly('text', 'data', 'data', '19/01/1990') ?> 
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+
+                                <div class="container-input-atualizacao-cadastral">
+                                    <?= label('email', 'Email') ?> 
+                                    <?= inputReadonly('text', 'email', 'email', 'jhon.f.kennedy@email.com') ?> 
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="campo-formulario">
-                        <?= label('telefone', 'Telefone') ?>
-                        <?= inputReadonly('text', 'telefone', 'telefone', '+55 (67) 9 9999-9999') ?>
+
+                </div>
+
+                <div class="container-endereco container-uper-readonly-secondary">
+                    <div class="titulo-endereco-atualizacao">
+                        <h1>Endereço</h1>
+                    </div>
+
+                    <div class="container-endereco-atualizacao-cadastral">
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('cep', 'CEP') ?>
+                            <?= inputReadonly('text', 'cep', 'cep', '123456-7') ?>
+                        </div>
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('logradouro', 'Logradouro') ?>
+                            <?= inputReadonly('text', 'logradouro', 'logradouro', 'Logradouro') ?>
+                        </div>
+                    </div>
+
+                    <div class="container-endereco-atualizacao-cadastral">
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('complemento', 'Complemento') ?>
+                            <?= inputReadonly('text', 'complemento', 'complemento', 'Ao lado do hospital municipal') ?>
+                        </div>
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('numero', 'Número') ?>
+                            <?= inputReadonly('text', 'numero', 'numero', '99') ?>
+                        </div>
+                    </div>
+
+                    <div class="container-endereco-atualizacao-cadastral">
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('bairro', 'Bairro') ?>
+                            <?= inputReadonly('text', 'bairro', 'bairro', 'Centro') ?>
+                        </div>
+
+                        <div class="container-input-atualizacao-cadastral">
+                            <?= label('cidade', 'Cidade') ?>
+                            <?= inputReadonly('text', 'cidade', 'cidade', 'Campo Grande') ?>
+                        </div>
+                    </div>
+
+                    <div class="container-uper-readonly-secondary">
+                        <?= label('complemento', 'Complemento') ?>
+                        <?= inputReadonly('text', 'complemento', 'complemento', 'Ao lado do hospital municipal') ?>
                     </div>
                 </div>
 
-                <div class="container-linha-campos">
-                    <div class="campo-formulario">
-                        <?= label('cpf', 'CPF') ?>
-                        <?= inputReadonly('text', 'cpf', 'cpf', '000.000.000-00') ?>
-                    </div>
-                    <div class="campo-formulario">
-                        <?= label('data', 'Nascimento') ?>
-                        <?= inputReadonly('text', 'data', 'data', '19/01/1990') ?>
-                    </div>
+                <div class="postagem-geral-div-btn">
+                     <div class="botao-excluir"><?= botao('excluir', 'Excluir', 'btnExcluirVoluntario', 'voluntariosOng.php') ?></div> 
                 </div>
 
-                <div class="container-linha-unica">
-                    <div class="campo-formulario">
-                        <?= label('email', 'Email') ?>
-                        <?= inputReadonly('text', 'email', 'email', 'jhon.f.kennedy@email.com') ?>
-                    </div>
-                </div>
+                <div id="modalConfirmacao" class="modal-overlay"> 
+                    <div class="modal-content"> 
+                        <p>Tem certeza que deseja excluir este voluntário?</p> 
 
-            </div>
-        </div>
-    </div>
+                        <div class="modal-botoes"> 
+                            <button id="btnConfirmarExclusao" class="botao botao-excluir">Sim</button> 
+                            <button id="btnCancelarExclusao" class="botao botao-cancelar">Cancelar</but> 
+                        </div> 
 
-    <div class="container-endereco container-readonly-secondary">
-        <div class="titulo-endereco-voluntario">
-            <h1>Endereço</h1>
-        </div>
+                    </div> 
 
-        <div class="container-endereco-voluntario">
-            <div class="container-input-endereco-voluntario">
-                <?= label('cep', 'CEP') ?>
-                <?= inputReadonly('text', 'cep', 'cep', '123456-7') ?>
-            </div>
-            <div class="container-input-endereco-voluntario">
-                <?= label('logradouro', 'Logradouro') ?>
-                <?= inputReadonly('text', 'logradouro', 'logradouro', 'Rua dos bobos') ?>
-            </div>
-        </div>
+                </div> 
 
-        <div class="container-endereco-voluntario">
-            <div class="container-input-endereco-voluntario">
-                <?= label('complemento', 'Complemento') ?>
-                <?= inputReadonly('text', 'complemento', 'complemento', 'Ao lado do hospital do carinho') ?>
-            </div>
-            <div class="container-input-endereco-voluntario">
-                <?= label('numero', 'Número') ?>
-                <?= inputReadonly('text', 'numero', 'numero', '0') ?>
-            </div>
-        </div>
-
-        <div class="container-endereco-voluntario">
-            <div class="container-input-endereco-voluntario">
-                <?= label('bairro', 'Bairro') ?>
-                <?= inputReadonly('text', 'bairro', 'bairro', 'Centro') ?>
-            </div>
-            <div class="container-input-endereco-voluntario">
-                <?= label('cidade', 'Cidade') ?>
-                <?= inputReadonly('text', 'cidade', 'cidade', 'Campo Grande') ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-readonly-footer">
-        <div class="botao-excluir-voluntario">
-            <div class="botao-excluir"><?= botao('excluir', 'Excluir', 'btnExcluirVoluntario', 'voluntariosOng.php') ?></div>
-        </div>
-    </div>
-</form>
-
+            </form>
         </div>
     </main>
-    
-    <div id="modalConfirmacao" class="modal-overlay">
-    <div class="modal-content">
-        <p>Tem certeza que deseja excluir este voluntário?</p>
-        <div class="modal-botoes">
-        <button id="btnConfirmarExclusao" class="botao botao-excluir">Sim</button>
-        <button id="btnCancelarExclusao" class="botao botao-cancelar">Cancelar</button>
-        </div>
-    </div>
-    </div>
-
-
     <?php require_once "../../../view/components/footer.php"; ?>
 </body>
 
 </html>
-
