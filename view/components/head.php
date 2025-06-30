ddf2eacac1866d253645787bab94ec29bf124ab4
LuanMendesMoura <luanmendesmoura091207@gmail.com>
2025-06-29 20:00:49 -0400

    adicionando um favicon no projeto

--- view/components/head.php
+++ view/components/head.php
@@ -12,6 +12,7 @@ session_start();
     <link rel="stylesheet" href="/together/view/assests/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=close" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=attach_file" />
+    <link rel="shortcut icon" href="/together/view/assests/images/components/logoTogetherLoginMobile.png" type="image/x-icon">
     
     <script src="https://kit.fontawesome.com/db29c2ca45.js" crossorigin="anonymous"></script>
 </head>
