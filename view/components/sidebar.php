0a293e1e52895588223a70ad3369ad50220d1783
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-07-09 20:16:56 -0400

    corrigindo erros da aside e dos filtros

--- view/components/sidebar.php
+++ view/components/sidebar.php
@@ -22,14 +22,14 @@
             <!-- ASIDE DA ONG -->
             <aside class="aside-area">
                 <div class="aside-content" id="sideOng">
-                    <a href="/together/view/pages/Ong/voluntariosOng.php" class="aside-button"> <i id="userIcon" class="fa-solid fa-users"></i> </a>
                     <a href="/together/view/pages/visaoSobreaOng.php" class="aside-button" id="categoriaButton"> <i id="visaoSobreOng" class="fa-solid fa-address-card"></i> </a>
-                    <a href="/together/view/pages/Ong/criarPostagemOng.php" class="aside-button"> <i id="criarIcon" class="fa-solid fa-square-plus"></i> </a>
-                    <a href="/together/view/pages/Ong/relatorioOng.php" class="aside-button"> <i id="relatorioIcon" class="fa-solid fa-book"></i> </a>
-                    <a href="/together/view/pages/Ong/validacaoVoluntario.php" class="aside-button"> <i id="voluntariosPendentes" class="fa-solid fa-user-clock"></i> </a>
                     <a href="/together/view/pages/Usuario/editarInformacoes.php" class="aside-button"> <i id="usuarioEdtarIcon" class="fa-solid fa-user-pen"></i></i> </a>
                     <a href="/together/view/pages/Usuario/usuarioOngsVoluntarias.php" class="aside-button"> <i id="userVoluntariadoIcon" class="fa-solid fa-handshake-simple"></i> </a>
                     <a href="/together/view/pages/Usuario/historicoDoacao.php" class="aside-button"> <i id="historicoIcon" class="fa-solid fa-clock"></i> </a>
+                    <a href="/together/view/pages/Ong/criarPostagemOng.php" class="aside-button"> <i id="criarIcon" class="fa-solid fa-square-plus"></i> </a>
+                    <a href="/together/view/pages/Ong/relatorioOng.php" class="aside-button"> <i id="relatorioIcon" class="fa-solid fa-book"></i> </a>
+                    <a href="/together/view/pages/Ong/validacaoVoluntario.php" class="aside-button"> <i id="voluntariosPendentes" class="fa-solid fa-user-clock"></i> </a>
+                    <a href="/together/view/pages/Ong/voluntariosOng.php" class="aside-button"> <i id="userIcon" class="fa-solid fa-users"></i> </a>
                 </div>
             </aside>
 
@@ -67,14 +67,14 @@
             <!-- ASIDE DA ONG -->
             <aside class="aside-area">
                 <div class="aside-content" id="sideOng">
-                    <a href="/together/view/pages/Ong/voluntariosOng.php" class="aside-button"> <i id="userIcon" class="fa-solid fa-users"></i> </a>
-                    <a href="/together/view/pages/Usuario/usuarioOngsVoluntarias.php" class="aside-button"> <i id="userVoluntariadoIcon" class="fa-solid fa-handshake-simple"></i> </a>
-                    <a href="/together/view/pages/Usuario/editarInformacoes.php" class="aside-button"> <i id="usuarioEdtarIcon" class="fa-solid fa-user-pen"></i></i> </a>
-                    <a href="/together/view/pages/Ong/criarPostagemOng.php" class="aside-button"> <i id="criarIcon" class="fa-solid fa-square-plus"></i> </a>
                     <a href="/together/view/pages/visaoSobreaOng.php" class="aside-button" id="categoriaButton"> <i id="visaoSobreOng" class="fa-solid fa-address-card"></i> </a>
+                    <a href="/together/view/pages/Usuario/editarInformacoes.php" class="aside-button"> <i id="usuarioEdtarIcon" class="fa-solid fa-user-pen"></i></i> </a>
+                    <a href="/together/view/pages/Usuario/usuarioOngsVoluntarias.php" class="aside-button"> <i id="userVoluntariadoIcon" class="fa-solid fa-handshake-simple"></i> </a>
+                    <a href="/together/view/pages/Usuario/historicoDoacao.php" class="aside-button"> <i id="historicoIcon" class="fa-solid fa-clock"></i> </a>
+                    <a href="/together/view/pages/Ong/criarPostagemOng.php" class="aside-button"> <i id="criarIcon" class="fa-solid fa-square-plus"></i> </a>
                     <a href="/together/view/pages/Ong/relatorioOng.php" class="aside-button"> <i id="relatorioIcon" class="fa-solid fa-book"></i> </a>
                     <a href="/together/view/pages/Ong/validacaoVoluntario.php" class="aside-button"> <i id="voluntariosPendentes" class="fa-solid fa-user-clock"></i> </a>
-                    <a href="/together/view/pages/Usuario/historicoDoacao.php" class="aside-button"> <i id="historicoIcon" class="fa-solid fa-clock"></i> </a>
+                    <a href="/together/view/pages/Ong/voluntariosOng.php" class="aside-button"> <i id="userIcon" class="fa-solid fa-users"></i> </a>
                 </div>
             </aside>
 
--- view/pages/Ong/visualizarVoluntarios.php
+++ view/pages/Ong/visualizarVoluntarios.php
@@ -11,27 +11,6 @@
         <div class="div-wrap-width">
             <h1 class="titulo-pagina">Dados do Voluntário</h1>
             <div class="formulario-perfil">
-            <div class="filtro">
-                    <div class="bloco-datas">
-                        <div class="filtro-por-mes">
-                            <?= label('data-inicio', 'Período') ?>
-                            <?= inputFilter('date', 'data-inicio', 'data-inicio') ?>
-                        </div>
-                        <div class="filtro-por-mes">
-                            <?= label('data-final', '&nbsp;') ?>
-                            <?= inputFilter('date', 'data-final', 'data-final') ?>
-                        </div>
-                        <div class="filtro-por-mes">
-                            <?= label('data-final', '&nbsp;') ?>
-                            <?= botao('primary', '✔') ?>
-                        </div>
-                    </div>
-
-                    <div class="bloco-pesquisa">
-                        <?= label('pesquisar', '&nbsp;') ?>
-                        <?= inputFilter('text', 'pesquisar', 'pesquisar', 'Pesquisar') ?>
-                    </div>
-                </div>
                 <form action="" method="POST">
                     <div class="container-perfil-voluntario">
                         <div class="div-logo">
