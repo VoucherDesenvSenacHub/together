46a6a1c9c44118fc9d657b5e668a9f66e1d9aaa4
Murilo460 <murilorocha.007@gmail.com>
2025-08-07 19:13:33 -0400

    up

--- model/UsuarioModel.php
+++ model/UsuarioModel.php
@@ -10,19 +10,6 @@ class UsuarioModel {
         $this->conn = $database->conectar();
     }
 
-
-    // id int not null auto_increment,
-    // nome varchar(60) not null,
-    // cpf varchar(14) not null,
-    // data_nascimento date,
-    // foto_de_perfil text,
-    // telefone varchar(16) not null,
-    // email varchar(50) not null primary key,
-    // senha varchar(8) not null,
-    // ativo bool,
-    // id_endereco int,
-    // foreign key(id_endereco) references enderecos(id)
-
     public function registrarUsuario($nome, $cpf, $telefone, $email, $senha ) {
         $sql = "INSERT INTO usuarios (nome, cpf, telefone, email, senhas) VALUES (
             :nome, :cpf, :telefone, :email, :senha)";
@@ -31,7 +18,7 @@ class UsuarioModel {
         $stmt->bindParam(':cpf', $cpf);
         $stmt->bindParam(':cpf', $telefone);
         $stmt->bindParam(':cpf', $email);
-        $stmt->bindParam(':senha', password_hash($senha, PASSWORD_BCRYPT));
+        $stmt->bindParam(':senha', var: password_hash($senha, PASSWORD_BCRYPT));
         
         return $stmt->execute();
     }
