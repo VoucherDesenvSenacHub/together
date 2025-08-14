540865dc74c7f7f82d9d930e3c10543938686c88
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-08-14 19:51:21 -0400

    Quinta Feira
    fotos devs

---
+++ controller/controllerCopyright.php
@@ -0,0 +1,67 @@
+<?php
+    $vetor = [
+        [
+            "nome" => "Eduardo Serafim",
+            "github" => "https://github.com/eduardoserafiim",
+            "linkedin" => "https://www.linkedin.com/in/eduardo-serafim-821649305/",
+            "imagem" => "../assests/images/Copyright/togetherteam/serafim.jpg"
+        ],
+        [
+            "nome" => "Henrico Queiroz",
+            "github" => "https://github.com/HenricQ",
+            "linkedin" => "https://www.linkedin.com/in/henrico-queiroz-725007325/",
+            "imagem" => "../assests/images/Copyright/togetherteam/queiroz.jpg"
+        ],
+        [
+            "nome" => "Vitor Galvão",
+            "github" => "https://github.com/vitorgalvao0",
+            "linkedin" => "https://www.linkedin.com/in/vitor-galv%C3%A3o-299891317/",
+            "imagem" => "../assests/images/Copyright/togetherteam/galvao.jpg"
+        ],
+        [
+            "nome" => "Antônio Victor",
+            "github" => "https://github.com/AntonioV1ctor",
+            "linkedin" => "https://www.linkedin.com/in/antoniov1ctor/",
+            "imagem" => "../assests/images/Copyright/togetherteam/victor.jpg"
+        ],
+        [
+            "nome" => "Luan Mendes",
+            "github" => "https://github.com/LuanMendesMoura",
+            "linkedin" => "https://www.linkedin.com/in/luan-m-26b8342bb/",
+            "imagem" => "../assests/images/Copyright/togetherteam/mendes.jpg"
+        ],
+        [
+            "nome" => "Gabrielle Faustino",
+            "github" => "https://github.com/GabrielleFaus",
+            "linkedin" => "https://www.linkedin.com/in/gabrielle-faustino-025aa1264/",
+            "imagem" => "../assests/images/Copyright/faustino.png"
+        ],
+        [
+            "nome" => "Rogério Vicente",
+            "github" => "https://github.com/rogeriovc",
+            "linkedin" => "?=usuarioNãoPossuiLinkedin",
+            "imagem" => "../assests/images/Copyright/togetherteam/vicente.jpg"
+        ],
+        [
+            "nome" => "Ariel David",
+            "github" => "https://github.com/ArielDavid79",
+            "linkedin" => "www.linkedin.com/in/ariel-leza-39680455",
+            "imagem" => "../assests/images/Copyright/togetherteam/david.jpg"
+        ],   
+        [
+            // https://github.com/RhyanSKomm
+            // www.linkedin.com/in/rhyan-komm-695699245
+            "nome" => "Rhyan Komm",
+            "github" => "?=usuarioNãoPossuiGithub",
+            "linkedin" => "?=usuarioNãoPossuiLinkedin",
+            "imagem" => "../assests/images/Copyright/togetherteam/komm.jpg"
+        ],
+        [
+            "nome" => "Allan Fernandes",
+            "github" => "https://github.com/All4nFernandes",
+            "linkedin" => "https://www.linkedin.com/in/allan-fernandes-665396230",
+            "imagem" => "../assests/images/Copyright/togetherteam/fernandes.jpg"
+        ],     
+    ]
+
+?>
--- view/pages/copyright.php
+++ view/pages/copyright.php
@@ -1,60 +1,13 @@
-<?php require_once "../../view/components/head.php" ?>
 <?php include "../components/card.php"; ?>
+<?php require_once "../../view/components/head.php" ?>
 <?php require_once "./../components/button.php" ?>
 <?php require_once "./../components/input.php" ?>
+<?php require_once "./../../controller/controllerCopyright.php" ?>
 
 <link rel="stylesheet" href="../assests/css/reset.css">
 <link rel="stylesheet" href="../assests/css/style.css">
 <link rel="stylesheet" href="../assests/css/pages/copyright.css">
 
-
-<?php
-    $vetor = [
-        [
-            "nome" => "Eduardo Serafim",
-            "github" => "https://github.com/eduardoserafiim",
-            "linkedin" => "https://www.linkedin.com/in/eduardo-serafim-821649305/",
-            "imagem" => "../assests/images/Copyright/serafim.png"
-        ],
-        [
-            "nome" => "Henrico Queiroz",
-            "github" => "https://github.com/HenricQ",
-            "linkedin" => "https://www.linkedin.com/in/henrico-queiroz-725007325/",
-            "imagem" => "../assests/images/Copyright/queiroz.png"
-        ],
-        [
-            "nome" => "Vitor Galvão",
-            "github" => "https://github.com/vitorgalvao0",
-            "linkedin" => "https://www.linkedin.com/in/vitor-galv%C3%A3o-299891317/",
-            "imagem" => "../assests/images/Copyright/galvao.png"
-        ],
-        [
-            "nome" => "Antônio Victor",
-            "github" => "https://github.com/AntonioV1ctor",
-            "linkedin" => "https://www.linkedin.com/in/antoniov1ctor/",
-            "imagem" => "../assests/images/Copyright/victor.png"
-        ],
-        [
-            "nome" => "Luan Mendes",
-            "github" => "https://github.com/LuanMendesMoura",
-            "linkedin" => "https://www.linkedin.com/in/luan-m-26b8342bb/",
-            "imagem" => "../assests/images/Copyright/mendes.png"
-        ],
-        [
-            "nome" => "Gabrielle Faustino",
-            "github" => "https://github.com/GabrielleFaus",
-            "linkedin" => "https://www.linkedin.com/in/gabrielle-faustino-025aa1264/",
-            "imagem" => "../assests/images/Copyright/faustino.png"
-        ],
-        [
-            "nome" => "Rogério Vicente",
-            "github" => "https://github.com/rogeriovc",
-            "linkedin" => "?=usuarioNãoPossuiLinkedin",
-            "imagem" => "../assests/images/Copyright/vicente.png"
-        ],       
-    ]
-
-?>
 <body>
     <div class="global">
         <div class="login-icon-group">  
