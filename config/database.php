f4c82d18b6c56bb665f8e91237016c7a7ccf6869
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-08-21 18:56:34 -0400

    database

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Database {
+        private $host = "localhost";
+        private $port = "3306";
+        private $dbName = "together";
+        private $user = "root";
+        private $password = "";
+
+
+    public function conectar()
+    {
+        $url = "mysql:host=$this->host;port=$this->port;dbname=$this->dbName";
+        $conn = new PDO($url, $this->user, $this->password);
+
+        return $conn;
+    }
+}
+
+
+?>
