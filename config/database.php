755e7f459fe00cda2346ebc402baacdffaceb38e
All4nFernandes <allandossantosfernandes@gmail.com>
2025-08-22 14:31:28 -0400

    colocando as configurações de acesso no banco remoto, tirado antes para testes.

--- config/database.php
+++ config/database.php
@@ -2,17 +2,17 @@
 
 
     class Database {
-        private $host = "localhost";
+        private $host = "10.78.0.215";
         private $port = "3306";
         private $dbName = "together";
         private $user = "root";
-        private $password = "";
+        private $password = "12345678";
 
 
     public function conectar()
     {
         $url = "mysql:host=$this->host;port=$this->port;dbname=$this->dbName";
-        $conn = new PDO($url, $this->user);
+        $conn = new PDO($url, $this->user, $this->password);
 
         return $conn;
     }
