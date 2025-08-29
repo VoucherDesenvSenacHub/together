3e6717fa615f61b0127be20e3d9d487422bb7022
Murilo460 <murilorocha.007@gmail.com>
2025-08-28 21:18:44 -0400

    feat:add search function in ongD

--- model/OngModel.php
+++ model/OngModel.php
@@ -19,4 +19,22 @@ class OngModel
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function findVoluntarioBySearch($nome_usuario)
+    {
+        $sql = "SELECT V.dt_associacao, U.nome
+                      FROM voluntarios V 
+                      JOIN usuarios U ON U.id = V.id_usuario 
+                      WHERE U.nome
+                      LIKE :nome_usuario
+                      ORDER BY V.dt_associacao DESC";
+
+        $stmt = $this->conn->prepare($sql);
+        $nome_usuario = '%' . $nome_usuario . '%';
+        $stmt->bindParam(':nome_usuario', $nome_usuario);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
 }
+
