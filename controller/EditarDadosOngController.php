91b8b10e3602b3f5e2abd8789c6ebbce4450cab5
All4nFernandes <allandossantosfernandes@gmail.com>
2025-09-11 21:37:52 -0400

    fix(ONG) Feito verificacao de data e adicionando verificao de cnpj existente e telefone.

--- controller/EditarDadosOngController.php
+++ controller/EditarDadosOngController.php
@@ -23,6 +23,30 @@ try {
         $erros[] = "Informe um email válido!";
     }
 
+    // verifica se é uma data valida e se não é maior que o ano atual
+    $VerificarData = explode('/', $_POST['data']);
+    if (count($VerificarData) === 3) {
+        $dia = (int) $VerificarData[0];
+        $mes = (int) $VerificarData[1];
+        $ano = (int) $VerificarData[2];
+        if (!checkdate($mes, $dia, $ano)) {
+            $erros[] = "Insira uma data válida!";
+        } elseif ($ano > date("Y")) {
+            $erros[] = "O ano não pode ser maior que o ano atual!";
+        }
+    } else {
+        $erros[] = "Insira uma data válida!";
+    }
+
+    $ongModel = new OngModel();
+    $_POST['cnpj'] = preg_replace('/\D/', '', $_POST['cnpj']);
+    $_POST['telefone'] = preg_replace('/\D/', '', $_POST['telefone']);
+    if (!empty($_POST['cnpj'] && $_POST['telefone'])) {
+        // echo ($cnpjLimpo);
+        $existe = $ongModel->verificaExisteDadosOng($_POST['cnpj'], $_POST['nome'], $_POST['telefone']);
+        var_dump($existe);
+    }
+
     // verfica se existe erro e exibe ao usuario
     if (!empty($erros)) {
         throw new Exception(implode("<br>", $erros));
