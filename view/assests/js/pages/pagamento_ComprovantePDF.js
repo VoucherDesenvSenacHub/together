ec26fceaeb713dee518cbc3f160887f565031df1
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-10-06 21:40:07 -0400

    Commit pagamento PDF
    Agora quando o usuario efetua o pagamento passa pelo js de PDF e no addevent ele faz o form parar e passar o que precisa depois continua o fluxo.

--- view/assests/js/pages/pagamento_ComprovantePDF.js
+++ view/assests/js/pages/pagamento_ComprovantePDF.js
@@ -1,29 +1,35 @@
-const botao_realizarPagamento = document.querySelector('#realizar_pagamento');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#form-pagamento');
 
-botao_realizarPagamento.addEventListener('click', () => {
-    const comprovante_nome = document.querySelector('#nome').value;
-    const comprovante_numero_cartao = document.querySelector('#numero').value;
-    const comprovante_valor = document.querySelector('#valor').value;
+    form.addEventListener('submit', function (event){
+        event.preventDefault();
 
-    const container_comprovante = document.createElement('div');
+        const nome = document.querySelector('#nome')?.value;
+        const numero_cartao = document.querySelector('#numero')?.value;
+        const numero = numero_cartao.slice(-4);
+        const valor = document.querySelector('#valor')?.value;
+
+        const container_comprovante = document.createElement('div');
+        container_comprovante.innerHTML = `
+        <h1 style="margin-bottom: 30px; font-weight: bold;">Comprovante Doação</h1>
+        
+        <p style="margin-bottom: 10px">Nome: <strong>${nome}</strong></p>
+        
+        <p style="margin-bottom: 10px">Cartão: **** **** **** <strong>${numero}</strong></p>
     
-    container_comprovante.innerHTML = `
-    <h1 style="margin-bottom: 30px; font-weight: bold;">Comprovante</h1>
-    
-    <p style="margin-bottom: 10px">Nome: <strong>${comprovante_nome.innerHTML}</strong></p>
-    
-    <p style="margin-bottom: 10px">Cartão: <strong>${comprovante_numero_cartao.innerHTML}</strong></p>
+        <p style="margin-bottom: 10px">Valor Pago: <strong>${valor}</strong></p>
+        `;
 
-    <p style="margin-bottom: 10px">Valor Pago: <strong>${comprovante_valor.innerHTML}</strong></p>
-    `;
+        const estilizacao = 
+        {
+            margin: [10, 10, 10, 10],
+            filename: "comprovante.pdf",
+            html2canvas: {scale: 2},
+            jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
+        };
 
-    const estilizacao = 
-    {
-        margin: [10, 10, 10, 10],
-        filename: "comprovante.pdf",
-        html2canvas: {scale: 2},
-        jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
-    };
-
-    html2pdf().set(estilizacao).from(container_comprovante).save();
+        html2pdf().set(estilizacao).from(container_comprovante).save().then(() => {
+            form.submit();
+        });
+    });
 });
