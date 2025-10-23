7428e65c1ccf7fbd11de73ab1045cac3d5669458
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-10-22 21:08:56 -0400

    alter table
    realizei o alter table no banco de dados local.

--- config/db/estrutura/03-att-tabela-doacoes.sql
+++ config/db/estrutura/03-att-tabela-doacoes.sql
@@ -1,1 +1,22 @@
+ALTER TABLE doacoes DROP COLUMN dt_doacao;
+
+ALTER TABLE doacoes DROP FOREIGN KEY doacoes_ibfk_1;
+ALTER TABLE doacoes DROP FOREIGN KEY doacoes_ibfk_2;
+
+ALTER TABLE doacoes DROP COLUMN id_usuario;
+ALTER TABLE doacoes DROP COLUMN id_ong;
+
+ALTER TABLE doacoes ADD COLUMN metodo_pagamento VARCHAR(50) NOT NULL;
+ALTER TABLE doacoes ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'PENDENTE';
+ALTER TABLE doacoes ADD COLUMN descricao TEXT;
+ALTER TABLE doacoes ADD COLUMN codigo_transacao VARCHAR(100);
+ALTER TABLE doacoes ADD COLUMN bandeira_cartao VARCHAR(20);
+ALTER TABLE doacoes ADD COLUMN ultimos_digitos VARCHAR(4);
+
+ALTER TABLE doacoes ADD COLUMN id_usuario INT NOT NULL;
+ALTER TABLE doacoes ADD COLUMN id_ong INT NOT NULL;
+
+ALTER TABLE doacoes ADD CONSTRAINT fk_doacoes_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id);
+ALTER TABLE doacoes ADD CONSTRAINT fk_doacoes_ong FOREIGN KEY (id_ong) REFERENCES usuarios(id);
+
 ALTER TABLE doacoes DROP COLUMN comprovante_path;
