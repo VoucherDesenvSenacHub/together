45999bfea9a3f1c524106a315d60867f4baef565
Henrico Queiroz <112563903+HenricQ@users.noreply.github.com>
2025-10-29 20:56:14 -0400

    Update PdfUtil.php

--- utils/PdfUtil.php
+++ utils/PdfUtil.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../config/database.php';
+require_once __DIR__ . '../config/database.php';
 
 class PdfUtil
 {
@@ -8,6 +8,18 @@ class PdfUtil
 
     public function __construct() {
         $database = new Database();
-        $this->$conn = $database->conectar();
+        $this->conn = $database->conectar();
+    }
+
+
+    public function procurarHistoricoDoacao($idUsuario)
+    {
+        $sql = "SELECT D.codigo_transacao, D.dt_doacao, U.nome, D.ultimos_digitos, D.valor 
+                FROM doacoes D JOIN usuarios U ON D.id_usuario = U.id 
+                WHERE id_usuario = :idUsuario";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':idUsuario', $idUsuario);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
