5dc7f3d7dd4d711e10fe0babe06caf8f923481dd
Henrico Queiroz <112563903+HenricQ@users.noreply.github.com>
2025-10-29 21:06:01 -0400

    Update PdfUtil.php

--- utils/PdfUtil.php
+++ utils/PdfUtil.php
@@ -12,13 +12,13 @@ class PdfUtil
     }
 
 
-    public function procurarHistoricoDoacao($idUsuario)
+    public function procurarHistoricoDoacao($idDoacao)
     {
         $sql = "SELECT D.codigo_transacao, D.dt_doacao, U.nome, D.ultimos_digitos, D.valor 
                 FROM doacoes D JOIN usuarios U ON D.id_usuario = U.id 
-                WHERE id_usuario = :idUsuario";
+                WHERE D.id = :idDoacao LIMIT 1";
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':idUsuario', $idUsuario);
+        $stmt->bindParam(':idDoacao', $idDoacao);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
