7161a00b0deb0ac2a7ff6f79192344ea9d99864c
Eduardo Serafim <eduardoserafiim05@gmail.com>
2025-10-30 19:52:25 -0400

    removendo composer.lock

--- services/RelatorioService.php
+++ services/RelatorioService.php
@@ -1,24 +1,12 @@
 <?php
-
-use Dompdf\Dompdf;
-
-require __DIR__ . '/../vendor/autoload.php';
-
 class RelatorioService
 {
-    public function gerarComprovanteDoacao($idDoacao)
+    public function gerarComprovanteDoacao()
     {
-        $id = $idDoacao;
-
         ob_start();
         require __DIR__ . '/../reports/comprovanteDoacao.php';
-        $relatorioComprovante = ob_get_clean();
-
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($relatorioComprovante);
-        $dompdf->setPaper('A4');
-        $dompdf->render();
-
-        $dompdf->stream();
+        $relatorioComprovanteHtml = ob_get_clean();
+        // Esse trecho eu tenho que mover para o utils, o service chama uma funcao util para gerar o relatorio
+        return ;
     }
 }
