a1d065a395d220c968a1124e75c8389eb5ba1965
Luan Moura <luan54098746@aluno.ms.senac.br>
2025-11-06 19:25:05 -0400

    arrumando a consulta sql de buscardoacoes

--- model/RelatorioModel.php
+++ model/RelatorioModel.php
@@ -12,10 +12,9 @@ class RelatorioModel
         $this->conn = $database->conectar();
     }
 
-    public function buscarDoacao($idDoacao) 
+    public function buscarDoacao($idDoacao)
     {
-        try
-        {
+        try {
             $sql = "SELECT D.codigo_transacao, D.dt_doacao, U.nome, D.ultimos_digitos, D.valor, O.razao_social, O.cnpj, O.telefone
                 FROM doacoes D 
                 JOIN usuarios U ON D.id_usuario = U.id
@@ -28,33 +27,28 @@ class RelatorioModel
             $stmt->bindParam(':idDoacao', $idDoacao);
             $stmt->execute();
             return $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        catch(Exception $e)
-        {
-            error_log('Erro ao fazer a busca: '. $e->getMessage());
+        } catch (Exception $e) {
+            error_log('Erro ao fazer a busca: ' . $e->getMessage());
         }
     }
 
-    public function buscarDoacoesOng($idOng, ){
-         try
-        {
+    public function buscarDoacoesOng($idOng,)
+    {
+        try {
             $sql = "SELECT D.dt_doacao, U.nome, D.valor, O.razao_social
                 FROM doacoes D
-                JOIN ongs O
-                ON O.id = :idOng
-                JOIN usuarios U
-                ON D.id_usuario = U.id
-                ORDER BY D.dt_doacao DESC
+                JOIN ongs O ON D.id_ong = O.id
+                JOIN usuarios U ON D.id_usuario = U.id
+                WHERE O.id = :idOng
+                ORDER BY D.dt_doacao DESC;
             ";
 
             $stmt = $this->conn->prepare($sql);
             $stmt->bindParam(':idOng', $idOng);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        catch(Exception $e)
-        {
-            error_log('Erro ao fazer a busca: '. $e->getMessage());
+        } catch (Exception $e) {
+            error_log('Erro ao fazer a busca: ' . $e->getMessage());
         }
     }
 }
