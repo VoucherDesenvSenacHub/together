c783f808f317082de43384c3ad964756e386dfb3
RhyanSKomm <rhyankomm@hotmail.com>
2025-11-25 19:09:34 -0400

    hotfix: insert em desnvolvedores

--- config/db/carga/01-carga-inicial.sql
+++ config/db/carga/01-carga-inicial.sql
@@ -19,3 +19,18 @@ INSERT INTO categorias_ongs (nome) VALUES
 ('Vida terrestre'),
 ('Paz, justiça e instituições eficazes'),
 ('Parcerias e meios de implementação');
+
+INSERT INTO desenvolvedores(nome, link_linkedin, link_github, link_foto) VALUES
+  ('Eduardo Serafim', 'https://www.linkedin.com/in/eduardo-serafim-821649', 'https://github.com/eduardoserafiim', '../assets/images/Copyright/togetherteam/serafim.jpg'),
+  ('Henrico Queiroz', 'https://www.linkedin.com/in/henrico-queiroz-7250073', 'https://github.com/HenricQ', '../assets/images/Copyright/togetherteam/queiroz.jpg'),
+  ('Vitor Galvão', 'https://www.linkedin.com/in/vitor-galv%C3%A3o-29982a', 'https://github.com/vitorgalvao0', '../assets/images/Copyright/togetherteam/galvao.jpg'),
+  ('Antônio Victor', 'https://www.linkedin.com/in/antoniov1ctor/', 'https://github.com/AntonioV1ctor', '../assets/images/Copyright/togetherteam/victor.jpg'),
+  ('Luan Mendes', 'https://www.linkedin.com/in/luan-m-26b8342bb/', 'https://github.com/LuanMendesMoura', '../assets/images/Copyright/togetherteam/mendes.jpg'),
+  ('Gabrielle Faustino', 'https://www.linkedin.com/in/gabrielle-faustino-025aaa', 'https://github.com/GabrielleFaus', '../assets/images/Copyright/togetherteam/faustino.jpg'),
+  ('Rogério Vicente', NULL, 'https://github.com/rogeriovc', '../assets/images/Copyright/togetherteam/vicente.jpg'),
+  ('Ariel David', 'https://www.linkedin.com/in/ariel-leza-39680455', 'https://github.com/ArielDavid79', '../assets/images/Copyright/togetherteam/david.jpg'),
+  ('Rhyan Komm', 'https://www.linkedin.com/in/rhyan-komm-695699245', 'https://github.com/RhyanSKomm', '../assets/images/Copyright/togetherteam/komm.jpg'),
+  ('Allan Fernandes', 'https://www.linkedin.com/in/allan-fernandes-6653962', 'https://github.com/All4nFernandes', '../assets/images/Copyright/togetherteam/fernandes.jpg'),
+  ('Murilo Dias', 'https://www.linkedin.com/in/murilo-rocha-4953a3333', 'https://github.com/Murilo460', '../assets/images/Copyright/togetherteam/dias.jpg'),
+  ('Rodrigo Ramalho', 'https://www.linkedin.com/in/rodrigo-garcia-ramalho-', 'https://github.com/RodrigoRamalho8', '../assets/images/Copyright/togetherteam/ramalho.jpg'),
+  ('Vitor Bueno', 'https://www.linkedin.com/in/vitor-bueno-novaga/', 'https://github.com/Vitor-Novaga', '../assets/images/Copyright/togetherteam/bueno.jpg');
