93e12f4d66c8303afdbc51fe5454608064ff5c79
RodrigoRamalho8 <rodrigogarcia.ramalho@gmail.com>
2025-11-25 19:56:45 -0400

    push alteacao no banco truncate tabelas e update desenvolvedores

---
+++ config/db/carga/02-update-desenvolvedores.sql
@@ -0,0 +1,7 @@
+UPDATE desenvolvedores
+SET link_linkedin = ''
+WHERE id = 
+
+UPDATE desenvolvedores
+SET link_linkedin = 'https://www.linkedin.com/in/rodrigo-garcia-ramalho-765757241/'
+WHERE id = 12
